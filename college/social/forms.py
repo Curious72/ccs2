70afc410118c28525ea053ad828a1147f3ce9b41
Curious72 <knowthyself2503@gmail.com>
2015-11-21 17:00:26 -0800

    Changes to be committed:

--- college/social/forms.py
+++ college/social/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class form1(forms.Form):
     name=forms.CharField()
-    year=forms.IntegerField()
+    #year=forms.IntegerField()
     branch=forms.CharField()
     email=forms.EmailField()
     password=forms.CharField()
--- college/social/views.py
+++ college/social/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-from social.models import users , announcements ,  complaints , assignments , curriculum
+from social.models import users
 from social.forms import form1
-from django.http import *
-from django.contrib.auth.models import User
-import datetime
-from djangoChat.models import ChatUser
-from django.contrib import auth
-from django.utils.timezone import now as utcnow
-
+from django.http import HttpResponseRedirect
 # Create your views here.
-
 def  signup(request):
     if "username" in request.session:
        return render(request,"redirect.html",{'name':request.session['username']})
@@ -21,76 +14,22 @@ def tq(request):
     form=form1(request.POST)
     if form.is_valid():
         form=form.cleaned_data
-        g=users(name=form['name'],password=form['password'], year=form['year'],email=form['email'],usertype=form['usertype'],branch=form['branch'])
-        ty= User.objects.create_user(form['name'],'knowthyself2503@gmail.com',form['password'])
+        g=users(name=form['name'],password=form['password'],email=form['email'],usertype=form['usertype'],branch=form['branch'])
         g.save()
-        uy=ChatUser(username=form['name'],is_chat_user=True)
-        uy.save()
-        username = form['name'] #retunr '' if no username
-        password = form['password']
-        assert False
         
-        return HttpResponseRedirect("/login")
+        return HttpResponseRedirect("/profile.html")
 
 def login(request):
     if "username" in request.session:
         return render(request,"redirect.html",{'name':request.session['username']})
-    
     else:
         return render(request,"login.html")
     
-def autho(request):
+def auth(request):
     try:
-        
+      
       j=users.objects.get(name=request.POST['name'],password=request.POST['password'])
-      k=users.objects.filter(name=request.POST['name'],password=request.POST['password'])
-      for i in k:
-          request.session['usertype']=i.usertype
-      
-      name=request.POST['name']
-      password=request.POST['password']
-      user=auth.authenticate(username=name,password=password)
-      auth.login(request,user)
-      cu = request.user.profile
-      cu.is_chat_user = True
-      cu.last_accessed = utcnow()
-      cu.save()
-      request.session['username']=name
-      request.session['password']=password
-      
-      return HttpResponseRedirect("/home1")
+      request.session['username']=request.POST['name']
+      return render(request,"profile.html")
     except users.DoesNotExist:
        return render(request,"login.html",{'errors':True})
-    
-def home(request,value):
-    if value =="1":
-        if 'text' in request.POST:
-            y=announcements(name=request.session['username'],text=request.POST['text'],time=datetime.datetime.now())
-            y.save()
-        j=announcements.objects.order_by("-time")
-    if value =="2":
-        if 'text' in request.POST:
-           y=complaints(name=request.session['username'],text=request.POST['text'],time=datetime.datetime.now())
-           y.save()
-        j=complaints.objects.order_by("-time")
-    if value =="3":
-        if 'text' in request.POST:
-           y=assignments(name=request.session['username'],text=request.POST['text'],time=datetime.datetime.now())
-           y.save()
-        j=assignments.objects.order_by("-time")
-    if value =="4":
-        if 'text' in request.POST:
-           y=curriculum(name=request.session['username'],text=request.POST['text'],time=datetime.datetime.now())
-           y.save()
-        j=curriculum.objects.order_by("-time")
-    print value
-    return render(request,"home.html",{'data':j,'value':value,'type':request.session['usertype']})
-
-def logout(request):
-    del request.session['username']
-    del request.session['usertype']
-    return HttpResponseRedirect("/welcome")
-
-
-def welcome(request):
-    return render(request,"welcome.html")
